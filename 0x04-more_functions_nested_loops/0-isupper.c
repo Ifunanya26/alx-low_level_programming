ab223228b38b41872e8d32f793a140219f1ec2b1
Ifunanya26 <lilyamber26@gmail.com>
2022-09-16 17:26:21 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,13 +3,9 @@
 
 
 /**
- *
- *  * _isupper - checks for uppercase character
- *
- *   * @c: character to be checked
- *
- *    * Return: 1 if true, 0 if false
- *
+ * _isupper - checks for uppercase character
+ * @c: character to be checked
+ * Return: 1 if true, 0 if false
  */
 
 
