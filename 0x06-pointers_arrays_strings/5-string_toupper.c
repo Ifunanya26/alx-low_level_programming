db22fcacf9e9b3857f839c52b66e188bd1632536
Ifunanya26 <lilyamber26@gmail.com>
2022-09-23 20:25:17 -0700

    cap string.c

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -2,11 +2,11 @@
 /**
  * string_toupper - changes all lowercase letters of a string
  * to uppercase
- * @s: input string.
- * Return: the pointer to dest.
+ * @s:  input string.
+ * Return:  the pointer to dest.
  */
 
-char *string_toupper(char *S)
+char *string_toupper(char *s)
 {
 	int count = 0;
 
