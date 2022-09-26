bda8a1a8332a33d965bcccfd5a24585c9db8729f
Ifunanya26 <lilyamber26@gmail.com>
2022-09-26 16:13:52 -0700

    crackme password

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,11 +1,9 @@
-
 #include "main.h"
 
 /**
  * set_string - sets the value of a pointer to a char
- * @s: source adress.
- * @to: target adress.
- * Return: no return.
+ * @s: address of a pointer
+ * @to: string to point to
  */
 void set_string(char **s, char *to)
 {
